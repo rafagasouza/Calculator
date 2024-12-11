340bdd4a2ee41bb6cffacf9f5426083038daeff7
Ráfaga Souza <devrafagamatos@gmail.com>
2024-12-11 09:35:55 -0300

    Project done

--- index.js
+++ index.js
@@ -41,7 +41,39 @@ input.addEventListener('keydown', (evento)=>{
 })
 
 function calculate(){
+    resultado.value = 'ERROR'
+    resultado.classList.add('error')
     const result = eval(input.value)
     resultado.value = result
+    resultado.classList.remove('error')
    
 }
+
+const themeSwitcher = document.getElementById('themeSwitcher').addEventListener('click', ()=>{
+    if(main.dataset.theme === 'dark'){
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme ='light'
+        
+    }else{
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#f1f5f9')
+        root.style.setProperty('--primary-color', '#4dff99')
+        main.dataset.theme ='dark'
+    }
+})
+
+document.getElementById('copyToClipboard').addEventListener('click', (ev)=>{
+    const button = ev.currentTarget
+    if(button.innerText === 'Copy'){
+        button.innerText = 'Copied'
+        button.classList.add('success')
+        navigator.clipboard.writeText(resultado.value)
+    }else{
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
